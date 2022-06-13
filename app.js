9336a6081e06fa8d236c8a3aadbd8d339aeb4258
Jonathan Pinilla <jonathan_pinilla@outlook.com>
2022-06-13 11:23:38 -0500

    Example finished, all working

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 var express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
-  methodOverride = require("method-override");
-mongoose = require("mongoose");
-
+  methodOverride = require("method-override"),
+  http = require("http"),
+  server = http.createServer(app),
+  mongoose = require("mongoose");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
+const { connectDatabase } = require("./db.mongo.js");
+
+connectDatabase();
+
 var router = express.Router();
+
+
+
+var TVShowCtrl = require("./controllers/tvshows");
+
+// API routes
+var tvshows = express.Router();
+
+tvshows
+  .route("/tvshows")
+  .get(TVShowCtrl.findAllTVShows)
+  .post(TVShowCtrl.addTVShow);
+
+tvshows
+  .route("/tvshows/:id")
+  .get(TVShowCtrl.findById)
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
+
+app.use("/api", tvshows);
+
+
 
 router.get("/", function (req, res) {
   res.send("Hello World!");
